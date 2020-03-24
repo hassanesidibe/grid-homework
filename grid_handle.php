0228d4dc3316aecfb2a21b139ab3616c67d794de
hassanesidibe <60410024+hassanesidibe@users.noreply.github.com>
2020-03-24 12:28:15 -0400

    Add files via upload

---
+++ grid_handle.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Grid view</title>
+</head>
+<body>
+	
+<h1>Grid</h1>
+<?php
+	
+	$row = $_POST['rows'];
+	$colum = $_POST['colums'];
+	
+	print"row is $row<br>";
+	print"colum is $colum";
+	
+	
+	?>
+</body>
+</html>
